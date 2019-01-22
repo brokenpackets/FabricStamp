59e8ffd8a784fc82b6da83bea9cb046bc1b56231
Tyler Conrad <36974147+brokenpackets@users.noreply.github.com>
2019-01-21 23:21:02 -0800

    Update ztp_FabricStamp.py

--- ztp_FabricStamp.py
+++ ztp_FabricStamp.py
@@ -156,9 +156,13 @@ def main():
     ### OPEN YAML file with device details.
     doc = yaml.load(yamlfile)
     spines = []
+    leaves = []
     for switch in doc.keys():
-        if doc[switch]['description'] == 'Spine':
-            spines.append(switch)
+        if doc[switch]['description'] in evpn_roles:
+            if doc[switch]['description'] == 'Spine':
+                spines.append(switch)
+            else:
+                leaves.append(switch)
     ethernetport = re.compile('^E[0-9]{1,2}')
     routedlinks = []
     for item in doc.keys():
@@ -219,11 +223,18 @@ def main():
                     if 'Lo100' in doc[item].keys():
                         tempconfiglet.append('      network '+doc[item]['Lo100'])
                 if device_role in evpn_roles:
-                    for spine in spines:
-                        spineip = doc[spine]['Lo0'].replace('/32','')
-                        spine_asn = doc[spine]['BGP-AS']
-                        tempconfiglet.append(evpnleaf.render(spineip=spineip,
-                                spine_asn=spine_asn, spine_Lo0=spine+'Lo0'))
+                    if device_role == 'Spine':
+                        for leaf in leaves:
+                            leafip = doc[leaf]['Lo0'].replace('/32','')
+                            leaf_asn = doc[leaf]['BGP-AS']
+                            f.write(evpnleaf.render(spineip=leafip,spine_asn=leaf_asn,
+                                                    spine_Lo0=leaf+'Lo0'))
+                    else:
+                        for spine in spines:
+                            spineip = doc[spine]['Lo0'].replace('/32','')
+                            spine_asn = doc[spine]['BGP-AS']
+                            tempconfiglet.append(evpnleaf.render(spineip=spineip,
+                                    spine_asn=spine_asn, spine_Lo0=spine+'Lo0'))
                     tempconfiglet.append(evpn_afv4_suffix.render())
                     if 'Lo0' in doc[item].keys():
                         tempconfiglet.append('      network '+doc[item]['Lo0'])
